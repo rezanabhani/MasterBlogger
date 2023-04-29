120830aecfc32e5ee99cb50a70b48b9dd5ed79ed
r <r@DESKTOP-2PJL3H7>
2023-04-29 18:46:51 +0430

    Comment Domain Implemented - Application Layer Structure Create

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using MB.Domain.ArticleAgg;
+
+namespace MB.Domain.CommentAgg
+{
+    public class Comment
+    {
+        public long Id { get;private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Message { get; private set; }
+        public int Status { get; private set; }//New = 0,Confirmed = 1,Canceled = 2
+        public DateTime CreationDate { get; private set; }
+        public long ArticleId { get; private set; }
+        public Article Article { get; private set; }
+
+        protected Comment()
+        {
+            
+        }
+
+        public Comment(string name, string email, string message, long articleId)
+        {
+            Name = name;
+            Email = email;
+            Message = message;
+            ArticleId = articleId;
+            CreationDate = DateTime.Now;
+            Status = Statuses.New;
+        }
+    }
+}
