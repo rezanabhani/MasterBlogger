b1f66490b7dd927ff38c1ac99d8f124d5a3ca170
r <r@DESKTOP-2PJL3H7>
2023-04-30 16:27:38 +0430

    List Comment Implemented

---
+++ MB.Application.Contracts/Comment/CommentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MB.Application.Contracts.Comment
+{
+    public class CommentViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get;  set; }
+        public string Message { get;  set; }
+        public int Status { get; set; }
+        public string CreationDate { get; set; }
+        public string Article { get; set; }
+    }
+}
--- MB.Infrastructure.EfCore/Repositories/CommentRepository.cs
+++ MB.Infrastructure.EfCore/Repositories/CommentRepository.cs
@@ -1,4 +1,9 @@
-﻿using MB.Domain.CommentAgg;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using MB.Application.Contracts.Comment;
+using MB.Domain.CommentAgg;
+using Microsoft.EntityFrameworkCore;
 
 namespace MB.Infrastructure.EfCore.Repositories
 {
@@ -16,6 +21,21 @@ namespace MB.Infrastructure.EfCore.Repositories
         {
             _context.Comments.Add(entity);
             _context.SaveChanges();
+        }
+
+        public List<CommentViewModel> GetList()
+        {
+            return _context.Comments.Include(x => x.Article)
+                .Select(x => new CommentViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Email = x.Email,
+                Message = x.Message,
+                Status = x.Status,
+                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                Article = x.Article.Title
+            }).ToList();
         }
     }
 }
